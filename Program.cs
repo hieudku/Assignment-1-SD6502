745b7b35b96cdf166388f8de1253c87081cf220d
Huba <hieu.dcu@gmail.com>
2024-03-05 20:20:50 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_SD6502
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
