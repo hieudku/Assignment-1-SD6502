ba12cf0511f31f821caac168eda823b429e12476
Huba <hieu.dcu@gmail.com>
2024-03-05 21:48:34 +1300

    Created classes and fields and properties. Added user interactions.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,60 @@ namespace Assignment_1_SD6502
     {
         static void Main(string[] args)
         {
+            char userContinue;
+
+
+            do
+            {
+                Console.WriteLine("Welcome to the online library. Select:");
+                Console.WriteLine("1. Display lists of items");
+                Console.WriteLine("2. Borrow items");
+                Console.WriteLine("3. Return items");
+
+
+
+
+                Console.WriteLine("Would you like to make additional request? (Y/N)");
+                userContinue = Convert.ToChar(Console.ReadLine());
+                userContinue = Char.ToUpper(userContinue);
+
+            } while (userContinue == 'Y');
+
         }
+    }
+    class Library
+    {
+        string itemCategory;
+        string itemName;
+        bool avalability;
+
+        public Library()
+        {
+
+        }
+
+        public string ItemName { get => itemName;}
+        public string ItemCategory { get => itemCategory;}
+        public bool Avalability { get => avalability; set => avalability = value; }
+
+        public void Borrow()
+        {
+
+        }
+    }
+
+    class Renewal : Library
+    {
+        
+    }
+
+    class Staffs
+    {
+
+    }
+
+    class Students
+    {
+
     }
 }
